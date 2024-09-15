d2c23195078219727847347fd9128ef01a51b87d
Edukskj <trindadev494@gmail.com>
2024-09-15 16:21:46 -0300

    Translate Fields and add Resource library (Spatie)

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -17,16 +17,45 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\Section::make([
-                    Forms\Components\TextInput::make('name') -> required() -> placeholder('Nome'),
-                    Forms\Components\TextInput::make('email') -> required() -> placeholder('exemplo@hotmail.com')->email(),
-                    Forms\Components\Toggle::make('active'),
+                    Forms\Components\Grid::make() ->schema([
+
+                        Forms\Components\TextInput::make('name')
+                            -> label('Nome')
+                            -> required()
+                            -> placeholder('Nome') 
+                            -> validationAttribute('Nome')
+                            ->rule('min:3'),
+
+                        Forms\Components\TextInput::make('email')
+                            -> label('E-mail') 
+                            -> required() 
+                            -> placeholder('exemplo@hotmail.com')
+                            ->email() 
+                            -> validationAttribute('E-mail'),
+
+                        Forms\Components\TextInput::make('password')
+                            -> label('Senha') 
+                            -> required(fn(string $context):bool=>$context=='create')
+                            -> password()
+                            -> confirmed(),
+
+                        Forms\Components\TextInput::make('password_confirmation')
+                            -> label('Confirmação de Senha') 
+                            -> required(fn(string $context):bool=>$context=='create')
+                            -> password() 
+                            -> validationAttribute('Confirmação de Senha'),
+
+                        Forms\Components\Toggle::make('active')
+                            -> label('Ativo'),
+
+                    ])
                 ])
             ]);
     }
@@ -35,17 +64,31 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name') -> searchable() -> sortable(),
-                Tables\Columns\TextColumn::make('email') -> searchable() -> sortable(),
-                Tables\Columns\IconColumn::make('active')->boolean(),
+
+                Tables\Columns\TextColumn::make('name')
+                    -> label('Nome') 
+                    -> searchable() 
+                    -> sortable(),
+
+                Tables\Columns\TextColumn::make('email') 
+                    -> label('E-mail')
+                    -> searchable() 
+                    -> sortable(),
+
+                Tables\Columns\IconColumn::make('active')
+                    -> label('Ativo')
+                    ->boolean(),
+
             ])
             ->filters([
+
                 Tables\Filters\SelectFilter::make('active') 
-                -> options([
-                    true => 'Active',
-                    false => 'Inactive'
-                ])
-                -> default('true')
+                    -> options([
+                        true => 'Ativo',
+                        false => 'Inativo'
+                    ])
+                    -> default('true')
+
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
